423bf35c6447aedb29fb5636089f3aefc51b64b5
Daniel David <daniel.david@students.ptechnyc.org>
2017-01-09 15:02:57 +0000

    01/09/2017

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //Write your function calls below this line
+//The Corners
+setColor(0,0, "orange");
+setColor(9,0, "blue")
+setColor(0,9, "black")
+setColor(9,9, "red")
+
+//The smiley face
+setColor(0,2, "gold")
+setColor(0,7, "gold")
+setColor(2,0, "gold")
+setColor(3,1, "gold")
+setColor(3,2, "gold")
+setColor(3,3, "gold")
+setColor(3,4, "gold")
+setColor(3,5, "gold")
+setColor(3,6, "gold")
+setColor(3,7, "gold")
+setColor(3,8, "gold")
+setColor(2,9, "gold")
+
+// Guyana Flag
+setColor(5,2, "red")
+setColor(6,2, "red")
+setColor(7,2, "red")
+setColor(8,2, "red")
+setColor(9,2, "red")
+setColor(6,3, "yellow")
+setColor(7,3, "yellow")
+setColor(8,3, "yellow")
+setColor(7,4, "yellow")
